a6638d0274a4e623d5785fe844293d9251312b2c
AlvyneZ <alvynew@gmail.com>
2023-06-21 22:40:30 +0300

    Not Nullable columns fix

--- models/place.py
+++ models/place.py
@@ -9,11 +9,11 @@ from models import storage_t
 place_amenity = Table('place_amenity', Base.metadata,
     Column(
         'place_id', String(60),
-        ForeignKey('places.id'), primary_key=True
+        ForeignKey('places.id'), primary_key=True, nullable=False
     ),
     Column(
         'amenity_id', String(60),
-        ForeignKey('amenities.id'), primary_key=True
+        ForeignKey('amenities.id'), primary_key=True, nullable=False
     )
 )
 
