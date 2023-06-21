e1d57417badd6e7b6474fa9590947b53e8b36a37
AlvyneZ <alvynew@gmail.com>
2023-06-21 22:45:36 +0300

    Adding relationship for amenities

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Amenity Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.place import place_amenity
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -12,5 +13,10 @@ class Amenity(BaseModel, Base):
     if storage_t == "db":
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
+        amenities = relationship(
+            "Place",
+            secondary=place_amenity,
+            viewonly=False
+        )
     else:
         name = ""
