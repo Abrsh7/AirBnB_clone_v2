b85800d7ba02ab2744d43e60cf956f50910f53a6
AlvyneZ <alvynew@gmail.com>
2023-06-22 20:39:30 +0300

    Adding none place_amenity for file_storage

--- models/place.py
+++ models/place.py
@@ -6,17 +6,18 @@ from sqlalchemy.orm import relationship
 
 from models import storage_t
 
-# if storage_t == "db":
-place_amenity = Table('place_amenity', Base.metadata,
-    Column(
-        'place_id', String(60),
-        ForeignKey('places.id'), primary_key=True, nullable=False
-    ),
-    Column(
-        'amenity_id', String(60),
-        ForeignKey('amenities.id'), primary_key=True, nullable=False
+place_amenity = None
+if storage_t == "db":
+    place_amenity = Table('place_amenity', Base.metadata,
+        Column(
+            'place_id', String(60),
+            ForeignKey('places.id'), primary_key=True, nullable=False
+        ),
+        Column(
+            'amenity_id', String(60),
+            ForeignKey('amenities.id'), primary_key=True, nullable=False
+        )
     )
-)
 
 
 class Place(BaseModel, Base):
