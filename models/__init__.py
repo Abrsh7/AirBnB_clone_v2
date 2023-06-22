d6b652ee11e02b265c29b8b328cf650f5942977d
AlvyneZ <60469361+AlvyneZ@users.noreply.github.com>
2023-06-22 09:24:30 +0300

    Importing file_storage if db not specified

--- models/__init__.py
+++ models/__init__.py
@@ -8,7 +8,7 @@ storage_t = getenv("HBNB_TYPE_STORAGE")
 if storage_t == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-elif storage_t == "file":
+else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
