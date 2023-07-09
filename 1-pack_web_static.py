70dcbed3ae1995e1732957d17557c35239c6d5f9
AlvyneZ <alvynew@gmail.com>
2023-07-09 20:28:11 +0300

    Using runsonce and print

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,10 +4,11 @@ Fabric script that generates a .tgz archive (gzip compression) from the
  contents of the web_static
 """
 from datetime import datetime
-from fabric.api import local, puts
+from fabric.api import local, runs_once
 from os.path import isdir
 
 
+@runs_once
 def do_pack():
     """Packs web_static to versions/web_static_{date}.tgz"""
     try:
@@ -15,7 +16,7 @@ def do_pack():
             local("mkdir versions")
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         file_name = "versions/web_static_{}.tgz".format(date)
-        # puts("Packing web_static to {}".format(file_name))
+        print("Packing web_static to {}".format(file_name))
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
     except Exception:
