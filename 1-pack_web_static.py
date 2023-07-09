322332d7dad09bb65996bf64dd5822dfbc97cba1
AlvyneZ <alvynew@gmail.com>
2023-07-09 18:44:55 +0300

    Compressing files

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive (gzip compression) from the
+ contents of the web_static
+"""
+from datetime import datetime
+from fabric.api import local, puts
+from os.path import isdir
+
+
+def do_pack():
+    """Packs web_static to versions/web_static_{date}.tgz"""
+    try:
+        if isdir("versions") is False:
+            local("mkdir versions")
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        puts("Packing web_static to {}".format(file_name))
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except Exception:
+        return None
